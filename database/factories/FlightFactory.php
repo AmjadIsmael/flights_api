0745ae14adb0935f1470a03ed6273d16fc79992f
AmjadIsmael <AmjadIsmael>
2024-06-20 11:17:38 +0300

    Create a Flight Model

---
+++ database/factories/FlightFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flight>
+ */
+class FlightFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'number' => $this->faker->unique()->numerify('####'),
+            'departure_city' => $this->faker->city(),
+            'arrival_city' => $this->faker->city(),
+            'departure_time' => $this->faker->time('H:i:s'),
+            'arrival_time' => $this->faker->time('H:i:s'),
+        ];
+    }
+}
