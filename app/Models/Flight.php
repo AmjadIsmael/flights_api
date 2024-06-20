209d88953de16419d8837f57074f72bfa790a33a
AmjadIsmael <AmjadIsmael>
2024-06-20 13:46:44 +0300

    Create a relatioship between tables

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -5,14 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Passenger;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Flight extends Model
 {
     use HasFactory;
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    public function passengers()
+    public function passengers(): BelongsToMany
     {
-        return $this->hasMany(Passenger::class);
+        return $this->belongsToMany(Passenger::class, 'passenger_flight');
     }
 }
--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\Flight;
 
 class Passenger extends Model
@@ -11,8 +12,8 @@ class Passenger extends Model
     use HasFactory;
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    public function flight()
+    public function flights(): BelongsToMany
     {
-        return $this->belongsTo(Flight::class);
+        return $this->belongsToMany(Flight::class, 'passenger_flight');
     }
 }
