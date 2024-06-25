102883ec4c28e3f09c480efc19ddb589dd2b99bb
AmjadIsmael <AmjadIsmael>
2024-06-25 12:15:38 +0300

    get flights associadted with each passenger using with

--- app/Http/Controllers/API/PassengerController.php
+++ app/Http/Controllers/API/PassengerController.php
@@ -12,17 +12,19 @@ class PassengerController extends Controller
 {
     public function index(Request $request)
     {
-        $passengers = QueryBuilder::for(Passenger::class)->allowedFilters([
-            AllowedFilter::exact('id'),
-            AllowedFilter::exact('first_name'),
-            AllowedFilter::exact('last_name'),
-            AllowedFilter::exact('email'),
-            AllowedFilter::exact('date_of_birth'),
-            AllowedFilter::exact('passport_expiry'),
+        $passengers = QueryBuilder::for(Passenger::class)
+            ->with('flights')
+            ->allowedFilters([
+                AllowedFilter::exact('id'),
+                AllowedFilter::exact('first_name'),
+                AllowedFilter::exact('last_name'),
+                AllowedFilter::exact('email'),
+                AllowedFilter::exact('date_of_birth'),
+                AllowedFilter::exact('passport_expiry'),
 
-        ])
+            ])
             ->allowedSorts(['id', 'first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry', 'created_at', 'updated_at'])
-            ->paginate($request->input('per_page', 10));
+            ->paginate($request->input('per_page', 100));
         return response()->json($passengers);
     }
 }
