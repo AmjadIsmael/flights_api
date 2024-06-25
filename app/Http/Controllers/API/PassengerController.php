ffc86d2785b6d398068627a6da15374bf6e929a8
AmjadIsmael <AmjadIsmael>
2024-06-25 10:59:24 +0300

    Create an Api for passengers with filtering,sorting and pagination and create a route to test them

--- app/Http/Controllers/API/PassengerController.php
+++ app/Http/Controllers/API/PassengerController.php
@@ -13,13 +13,16 @@ class PassengerController extends Controller
     public function index(Request $request)
     {
         $passengers = QueryBuilder::for(Passenger::class)->allowedFilters([
-                AllowedFilter::exact('id'),
-                AllowedFilter::exact('first_name'),
-                AllowedFilter::exact('last_name'),
-                AllowedFilter::exact('email'),
+            AllowedFilter::exact('id'),
+            AllowedFilter::exact('first_name'),
+            AllowedFilter::exact('last_name'),
+            AllowedFilter::exact('email'),
+            AllowedFilter::exact('date_of_birth'),
+            AllowedFilter::exact('passport_expiry'),
 
-            ])
-            ->allowedSorts(['id', 'first_name', 'last_name', 'email', 'created_at', 'updated_at'])
-            ->paginate();
+        ])
+            ->allowedSorts(['id', 'first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry', 'created_at', 'updated_at'])
+            ->paginate($request->input('per_page', 10));
+        return response()->json($passengers);
     }
 }
